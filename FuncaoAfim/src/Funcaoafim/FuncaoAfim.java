1f791f05c5ec513330f033d89facde42ab08ea1b
GustavoMartinsdosSantos <gustavo_mts2005@hotmail.com>
2020-06-10 11:42:47 -0300

    Solved the InputMismatchException

--- FuncaoAfim/src/Funcaoafim/FuncaoAfim.java
+++ FuncaoAfim/src/Funcaoafim/FuncaoAfim.java
@@ -1,5 +1,6 @@
 package Funcaoafim;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FuncaoAfim {
@@ -15,70 +16,77 @@ public class FuncaoAfim {
 		double x;
 		double y;
 		
-		while (option.equals("1")) {
-			System.out.println("(OBS: o programa irá calcular uma possível função para esses pontos)");
-			System.out.print("Digite o  ponto (x,y): ");
-			x = input.nextInt();
-		
-			while (x == 0) {
-				System.out.print("x deve ser diferente de 0. Tente novamente:");
-				x = input.nextInt();
-			} // x deve ser diferente de 0 para o programa conseguir fazer a divisão 
-			  // e calcular o valor de a. Num / 0 == ?
+		try {
+			while (option.equals("1")) { 
+				System.out.println("(OBS: o programa irá calcular uma possível função para esses pontos)");
+				System.out.print("Digite o  ponto (x,y): ");
+				x = input.nextDouble();
 			
-			y = input.nextInt();
-			System.out.println("(" + x + "," + y + ")"); 
-			
-			System.out.print("Digite o valor b:");
-			b = input.nextInt();
-			a = (y - b) / x;
-		
-			if (y == b) 
-				System.out.println("y = " + b + " -> Função constante");
-			else if (a > 0 && b == 0)
-				if (a == 1)
-					System.out.println("y = x -> Função identidade.");
-				else 
-					System.out.println("y = " + a + "x -> Função linear crescente");
-			else if (a < 0 && b == 0)
-				System.out.println("y = " + a + "x -> Função linear decrescente");
-			else if (a > 0)
-				System.out.println("y = " + a + "x + " + b + " -> Função crescente");
-			else if (a < 0)
-				System.out.println("y = " + a + "x + " + b + " -> Função decrescente");
-			else
-				System.out.println("Não é função.");
-			
-			System.out.print("Digite 1 para continuar calculando e 2 para usar essa função: ");
-			option = input.next();
-		}
-		
-		while(option.equals("2")) {
-			System.out.print("Digite o valor de x: ");
-			x = input.nextInt();
-			
-			if (a == 0 && b == 0) {
-				System.out.print("f(x) = ");
-				a = input.nextInt();
+				while (x == 0) {
+					System.out.print("x deve ser diferente de 0. Tente novamente:");
+					x = input.nextDouble();
+				} // x deve ser diferente de 0 para o programa conseguir fazer a divisão 
+				  // e calcular o valor de a. Num / 0 == ?
 				
-				System.out.print("f(x) = " + a + "x + ");
-				b = input.nextInt();
+				y = input.nextDouble();
+				System.out.println("(" + x + "," + y + ")"); 
 				
-				System.out.println("f(x) = " + a + "x + " + b);
+				System.out.print("Digite o valor b:");
+				b = input.nextDouble();
+				a = (y - b) / x;
+			
+				if (y == b) 
+					System.out.println("y = " + b + " -> Função constante");
+				else if (a > 0 && b == 0)
+					if (a == 1)
+						System.out.println("y = x -> Função identidade.");
+					else 
+						System.out.println("y = " + a + "x -> Função linear crescente");
+				else if (a < 0 && b == 0)
+					System.out.println("y = " + a + "x -> Função linear decrescente");
+				else if (a > 0)
+					System.out.println("y = " + a + "x + " + b + " -> Função crescente");
+				else if (a < 0)
+					System.out.println("y = " + a + "x + " + b + " -> Função decrescente");
+				else
+					System.out.println("Não é função.");
 				
-				System.out.println("f(x) = " + (a * x + b));
+				System.out.print("Digite 1 para continuar calculando e 2 para usar essa função: ");
+				option = input.next();
+			} 
+				
+			if (option.equals("2")) {
+				System.out.print("Digite o valor de x: ");
+				x = input.nextDouble();
+					
+				if (a == 0 && b == 0) {
+					System.out.print("f(x) = ");
+					a = input.nextDouble();
+						
+					System.out.print("f(x) = " + a + "x + ");
+					b = input.nextDouble();
+						
+					System.out.println("f(x) = " + a + "x + " + b);
+						
+					System.out.println("f(x) = " + (a * x + b));
+				} else 
+					System.out.println("f(x) = " + (a * x + b));
+				
+				a = 0;
+				b = 0;
+				x = 0;
+					// reinicialização das variáveis
+				main(null);
 			} else 
-				System.out.println("f(x) = " + (a * x + b));
-			
-			a = 0;
-			b = 0;
-			x = 0;
-			// reinicialização das variáveis
-			
-			main(null);
-		} 
-		
-		System.out.println("Obrigado por usar o programa!!");
+				System.out.println("Obrigado por usar o programa!!");
+	}
+	catch (InputMismatchException e) {
+		main(null);
+	}
+	catch (Exception e) {
+		System.out.println("ERRO!");
+		e.printStackTrace();
+	}
 			
 		input.close();
 	}	
